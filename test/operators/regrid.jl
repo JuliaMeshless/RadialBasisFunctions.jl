f675246fcb2527164fbef9a18be78fc4463dd022
Kyle Beggs <beggskw@gmail.com>
2026-01-07 22:14:45 -0500

    add some coverage for new constructors

--- test/operators/regrid.jl
+++ test/operators/regrid.jl
@@ -12,7 +12,24 @@ x = SVector{2}.(HaltonPoint(2)[1:N])
 y = f.(x)
 
 x2 = map(x -> SVector{2}(rand(2)), 1:100)
-r = regrid(x, x2, PHS(3; poly_deg=2))
-@test mean_percent_error(r(y), f.(x2)) < 0.1
 
-@test RadialBasisFunctions.print_op(r.ℒ) == "regrid"
+@testset "Positional Basis Constructor" begin
+    r = regrid(x, x2, PHS(3; poly_deg=2))
+    @test mean_percent_error(r(y), f.(x2)) < 0.1
+end
+
+@testset "Keyword Constructor" begin
+    # Test regridding.jl lines 39-41: primary keyword constructor
+    r_kw = regrid(x, x2)  # Uses default basis
+    @test r_kw isa RadialBasisOperator
+    @test mean_percent_error(r_kw(y), f.(x2)) < 0.1
+
+    # Test with explicit keyword basis
+    r_kw2 = regrid(x, x2; basis=PHS(5; poly_deg=3))
+    @test r_kw2 isa RadialBasisOperator
+end
+
+@testset "Printing" begin
+    r = regrid(x, x2, PHS(3; poly_deg=2))
+    @test RadialBasisFunctions.print_op(r.ℒ) == "regrid"
+end
